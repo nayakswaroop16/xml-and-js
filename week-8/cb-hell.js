24a23a60518fd8c55ac9974180440773f0ab6c8d
nayakswaroop16 <83548441+nayakswaroop16@users.noreply.github.com>
2022-03-08 20:34:06 -0500

    activity

--- week-8/cb-hell.js
+++ week-8/cb-hell.js
@@ -4,53 +4,47 @@
  * 3. Make sure the "Finish" is logged after all the data is converted
  */
 
-function timeout(ms, callback) {
-  return new Promise(function (resolve) {
-    setTimeout(function () {
-      resolve();
-      callback();
-    }, ms);
-  });
-}
+const timeout = async (ms) => {
+	return new Promise((resolve) => {
+		resolve();
+	}, ms);
+};
 
-function generateRandomNumber() {
-  return Math.floor(Math.random() * 40);
-}
+const generateRandomNumber = () => {
+	return Math.floor(Math.random() * 40);
+};
 
-function generateData(callback) {
-  timeout(1000, function () {
-    const data = Array.from({ length: 20 }, generateRandomNumber);
-    callback(data);
-  });
-}
+const generateData = async () => {
+	await timeout(200);
+	const data = Array.from({ length: 20 }, generateRandomNumber);
+	return data;
+};
 
-function convertToFeet(meters, callback) {
-  const feet = meters * 3.2808;
-  timeout(3500, function () {
-    callback(feet);
-  });
-}
+const convertToFeet = (meters) => {
+	const feet = meters * 3.2808;
+	return new Promise((resolve) => {
+		resolve(feet);
+	});
+};
 
-function processData(data, callback) {
-  data.map(function (value) {
-    callback(value);
-  });
-}
+const processData = async (data, converterFn) =>
+	await Promise.all(
+		data.map(async (value) => {
+			return { val: value, res: await converterFn(value) };
+		})
+	);
 
-function logResult(meters, feet) {
-  console.log(`Converted ${meters}m to ${feet}ft`);
-}
+const logResult = (meters, feet) =>
+	console.log(`Converted ${meters}m to ${feet}ft`);
 
-function main() {
-  console.log("Start");
-  generateData(function (data) {
-    processData(data, function (value) {
-      convertToFeet(value, function (result) {
-        logResult(value, result);
-      });
-    });
-  });
-  console.log("Finish");
-}
+const main = async () => {
+	console.log("Start");
+	let data = await generateData();
+	let processedData = await processData(data, convertToFeet);
+	processedData.forEach((element) => {
+		logResult(element.val, element.res);
+	});
+	console.log("Finish");
+};
 
 main();
--- week-8/sync.js
+++ week-8/sync.js
@@ -1,44 +1,62 @@
+const timeout = (delay) => {
+	return new Promise(function (resolve) {
+		setTimeout(resolve, delay);
+	});
+};
+
 function inc(a) {
-  return a + 1;
+	return new Promise((resolve) => {
+		timeout(300).then(() => {
+			resolve(a + 1);
+		});
+	});
 }
 
 const sum = function (a, b) {
-  return a + b;
+	return new Promise((resolve) => {
+		timeout(300).then(() => {
+			resolve(a + b);
+		});
+	});
 };
 
 const max = (a, b) => (a > b ? a : b);
 
-const avg = (a, b) => {
-  const s = sum(a, b);
-  return s / 2;
-};
+const avg = (a, b) =>
+	timeout(300)
+		.then(() => sum(a, b))
+		.then((s) => s / 2);
 
 const obj = {
-  name: "Marcus Aurelius",
-  split(sep = " ") {
-    return this.name.split(sep);
-  },
+	name: "Marcus Aurelius",
+	split(sep = " ") {
+		return this.name.split(sep);
+	},
 };
 
 class Person {
-  constructor(name) {
-    this.name = name;
-  }
+	constructor(name) {
+		this.name = name;
+	}
 
-  static of(name) {
-    return new Person(name);
-  }
+	static of(name) {
+		return new Person(name);
+	}
 
-  split(sep = " ") {
-    return this.name.split(sep);
-  }
+	split(sep = " ") {
+		return this.name.split(sep);
+	}
 }
 
 const person = Person.of("Marcus Aurelius");
 
-console.log("inc(5) =", inc(5));
-console.log("sum(1, 3) =", sum(1, 3));
-console.log("max(8, 6) =", max(8, 6));
-console.log("avg(8, 6) =", avg(8, 6));
-console.log("obj.split() =", obj.split());
-console.log("person.split() =", person.split());
+const main = async () => {
+	console.log("inc(5) =", await inc(5));
+	console.log("sum(1, 3) =", await sum(1, 3));
+	console.log("max(8, 6) =", await max(8, 6));
+	console.log("avg(8, 6) =", await avg(8, 6));
+	console.log("obj.split() =", obj.split());
+	console.log("person.split() =", person.split());
+};
+
+main();
