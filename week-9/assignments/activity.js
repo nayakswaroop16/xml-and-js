94254973c350cb5b643ad711f0e8e92859d64558
nayakswaroop16 <83548441+nayakswaroop16@users.noreply.github.com>
2022-03-14 20:22:40 -0400

    activity

---
+++ week-9/assignments/activity.js
@@ -0,0 +1,44 @@
+const loadData = (path) =>
+	new Promise((resolve) => {
+		const xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = ({ target }) => {
+			if (target.readyState == 4 && target.status == 200) {
+				resolve(JSON.parse(target.response));
+			}
+		};
+		xhttp.open("GET", path, true);
+		xhttp.send();
+	});
+
+const renderData = (data, searchTerm) => {
+	const table = document.getElementById("html-table");
+	if (searchTerm) {
+		data = data.filter(
+			({ first_name, last_name, email, gender, ip_address }) =>
+				last_name.toLowerCase().includes(searchTerm) ||
+				first_name.toLowerCase().includes(searchTerm) ||
+				email.toLowerCase().includes(searchTerm) ||
+				gender.toLowerCase().includes(searchTerm) ||
+				ip_address.includes(searchTerm)
+		);
+	}
+	const rows = data.reduce(
+		(acc, row) =>
+			acc +
+			`<tr id="table-row-${row.id}"><td>${row.id}</td><td>${row.first_name}</td><td>${row.last_name}</td><td>${row.email}</td><td>${row.gender}</td><td>${row.ip_address}</td></tr>`,
+		``
+	);
+	table.innerHTML = rows;
+};
+
+const onSubmit = (event) => {
+	event.preventDefault();
+	const searchTerm = event.target.search.value;
+	loadData("./data.json").then((data) => renderData(data, searchTerm));
+};
+
+loadData("./data.json").then((data) => renderData(data));
+
+const onReset = () => {
+	loadData("./data.json").then((data) => renderData(data));
+};
